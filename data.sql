15611661d611320854efee1f2173bfc77daa1508
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-07-25 12:23:24 +0100

    filles

---
+++ data.sql
@@ -0,0 +1,18 @@
+
+
+
+insert into fermes
+(nomDomaine,logo,	fullNameG,	cin,	contactG,	SAT,	SAU	,fixe,	fax	,GSM1	,GSM2	,email	,siteWeb,	Douar,	Cercle	,province,	region	,adresse,	GPS	,ville
+)
+VALUES
+('Ferme Elmernissi' ,'	Ferme/Ferme Elmernissi.jpg','	Ahmed El Mansouri','	B123456',	'212,600,123,456',	77	,50	,'+212 522 123 456',' +212 522 123 450	',' +212 522 123 450	',' +212 600 123 456','Ferme-Elmernissi@gmail.com','Ferme-Elmernissi.ma','	Aït Ouallal',	'Sidi Harazem','	Moulay Yacoub',	'Fès-Meknès','','332G+M3H, Fès',	'FES')
+
+('la ferme elkhalifi' ,' 	Ferme/la ferme elkhalifi.jpg','Fatima Zouaoui','B987654	','212,661,234,567',	90	,80,		'+212 537 234 567',' +212 537 234 560 ','+212 537 234 560','+212 661 234 567',	'ferme-elkhalifi@gmail.com',	'ferme-elkhalifi.ma',	'El Menzeh','Fès',	'Fès',	'Fès-Meknès','Ain Chekafe Karaouine, Fès','',		'FES'),
+('ferma chraibi'	 ,'Ferme/ferma chraibi.jpg','	Rachid Benjelloun','	CD456789',	'212,622,345,678',145,	120	,	 '+212 535 345 678'	, '+212 535 345 670'	, '+212 535 345 670'	,'+212 622 345 678','ferma-chraibi@gmail.com',	'ferma-chraibi.ma',	'Aït Sebaâ Oudaïs','	Sefrou',	'Sefrou','Fès-Meknès'	,'',	'225G+87F, Fès'	,'FES')
+
+--------------------
+Sol équilibré	Sol loam
+ Terre fertile	Sol silty
+Terre noire 	Sol humus
+ Terre légère	 Sol sandy loam
+
